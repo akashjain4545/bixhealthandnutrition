54c56afadd68155ce2770231e90e80f4b3d7703e
akashjain4545 <bixhealthandnutrition@gmail.com>
2025-06-07 14:24:23 +0530

    Initial project structure

---
+++ deepseek_bash_20250607_6391a7.sh
@@ -0,0 +1,10 @@
+# Add all files
+git add .
+
+# Make initial commit
+git commit -m "Initial project structure"
+
+# Create GitHub repository (do this on github.com first)
+git remote add origin https://github.com/akashjain4545/bixhealthandnutrition.git
+git branch -M main
+git push -u origin main
